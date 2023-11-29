de680ddc8a046b804584b738a48b810d135239c1
Dale Fielding <dalefielding28@gmail.com>
2023-11-29 15:26:24 +0000

    Add code examples to demonstrate the purpose of the mpay method

--- map.js
+++ map.js
@@ -5,16 +5,28 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5]
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results)
 
 // Using map()
+const multiplyByTwo = function(num) { // the callback function is usually just passed as a parameter without being assigned to a variable first.
+  return num * 2
+} 
 
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
+const mapSimplified = nums.map(function (num) {return num * 2});
+console.log(mapSimplified);
 
-
-// Simplfied w/ map() + arrow function
-
+// Simplified w/ map() + arrow function
+const mapArrow = nums.map(num => num * 2);
+console.log(mapArrow);
 
 // With objects:
 const students = [
@@ -37,3 +49,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map((student) => [student.id, student.name]);
+console.log(studentsWithIds);
